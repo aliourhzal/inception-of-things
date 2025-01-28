bd5c23d80bafbb8c3aa740dc64f879135630a3cc
aliourhzal <aourhzal@student.1337.ma>
2025-01-28 10:32:42 +0100

    wait for all argocd pods to run before creating the app

--- p2/script.sh
+++ p2/script.sh
@@ -10,26 +10,17 @@ kubectl create namespace dev
 echo -e "\033[1;32mInstalling ArgoCD...\033[0m"
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+while [ true ]: do
+    i=$(kubectl get pod -n argocd | grep "Running" | wc -l)
+    if [ $i = 7 ]
+    then
+        break
+    else
+        echo -en "\r\033[1;32mInitializing cluster...\033[0m"
+    fi
+done;
 
-ARGOCD_REPO_SERVER_POD=$(kubectl get pods -n argocd | grep "^argocd-repo-server" | awk '{print $1}')
-ARGOCD_REPO_SERVER_STATUS=$(kubectl describe pod $ARGOCD_REPO_SERVER_POD -n argocd | grep "^Status" | awk '{print $2}')
-
-echo -e "\033[1;32mStarting ArgoCD repo server...\033[0m"
-while [ $ARGOCD_REPO_SERVER_STATUS != "Running" ]; do
-    sleep 1
-    ARGOCD_REPO_SERVER_STATUS=$(kubectl describe pod $ARGOCD_REPO_SERVER_POD -n argocd | grep "^Status" | awk '{print $2}')
-done
-
-ARGOCD_SERVER_POD=$(kubectl get pods -n argocd | grep "^argocd-server" | awk '{print $1}')
-ARGOCD_SERVER_STATUS=$(kubectl describe pod $ARGOCD_SERVER_POD -n argocd | grep "^Status" | awk '{print $2}')
-
-echo -e "\033[1;32mStarting ArgoCD server...\033[0m"
-while [ $ARGOCD_SERVER_STATUS != "Running" ]; do
-    sleep 1
-    ARGOCD_SERVER_STATUS=$(kubectl describe pod $ARGOCD_SERVER_POD -n argocd | grep "^Status" | awk '{print $2}')
-done
-
-echo -e "\033[1;32mCreating ArgoCD app...\033[0m"
+echo -e "\n\033[1;32mCreating ArgoCD app...\033[0m"
 kubectl apply -f application.yaml
 
 # ARGOCD_PASSWD=$(argocd admin initial-password -n argocd | head -n 1)
