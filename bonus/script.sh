84afdb5b071353acadeea9fba1cc02374fc9859f
aliourhzal <aourhzal@student.1337.ma>
2025-03-25 01:40:08 +0000

    working version

--- bonus/script.sh
+++ bonus/script.sh
@@ -3,35 +3,35 @@
 echo -e "\033[1;32mCreating cluster...\033[0m"
 k3d cluster create inception
 
-echo -e "\033[1;32mCreating namespaces...\033[0m"
-kubectl create namespace argocd
-kubectl create namespace dev
+echo -en "\033[1;32mCreating namespaces...\033[0m"
 kubectl create namespace gitlab
 
-echo -e "\033[1;32mInstalling Gitlab...\033[0m"
+echo -en "\033[1;32mInstalling Gitlab...\033[0m"
+helm repo add gitlab https://charts.gitlab.io/
+helm repo update
 helm upgrade --install gitlab gitlab/gitlab \
   --namespace gitlab \
   -f gitlab-values.yaml
 
-while [ true ]; do
-    i=$(kubectl get pod -n gitlab | grep "Running" | wc -l)
-    if [ $i = 23 ]
-    then
-        break
-    else
-        echo -en "\r\033[1;32mInitializing cluster...\033[0m"
-    fi
-done
+# sleep 30
+# echo -en "\r\033[1;32mInitializing cluster...\033[0m"
+# while [ true ]; do
+#     i=$(kubectl get pods -n gitlab | grep "Running" | wc -l)
+#     if [ $i = 23 ]
+#     then
+#         break
+#     fi
+# done
 
+# echo -en "\r\033[1;32mCluster initialized...\033[0m"
+# GITLAB_PASSWD=$(kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -o jsonpath='{.data.password}' | base64 --decode)
 
-ARGOCD_PASSWD=$(kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -o jsonpath='{.data.password}' | base64 --decode)
+# echo "***********************************************************************************"
+# echo "*                                                                                 *"
+# echo "*  login: root                                                                    *"
+# echo "*  password: $GITLAB_PASSWD     *"
+# echo "*  url: localhost:8080                                                            *"
+# echo "*                                                                                 *"
+# echo "***********************************************************************************"
 
-echo "***********************************************************************************"
-echo "*                                                                                 *"
-echo "*  login: root                                                                    *"
-echo "*  password: $ARGOCD_PASSWD      *"
-echo "*  url: localhost:8080                                                            *"
-echo "*                                                                                 *"
-echo "***********************************************************************************"
-
-// kubectl port-forward svc/gitlab-webservice-default -n gitlab 8080:8080
+# #   
